254165c3076bb3c80f7e9a975eafde97f05441a2
TheTommyHsieh@gmail.com <TheTommyHsieh@gmail.com>
2021-09-01 23:31:51 +0800

    [^] partial class.

---
+++ BlazorServer/Pages/FetchData.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorServer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public partial class FetchData
+    {
+        public IWeatherForecastService _forecastService { get; } = new NewWeatherForecastService();
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await _forecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
